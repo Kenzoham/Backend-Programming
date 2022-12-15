62a4eaa70ded7fd0f8750402129ac6d34ae530e1
Abdullah ilham <91928537+Kenzoham@users.noreply.github.com>
2022-12-16 00:03:56 +0700

    Update AnimalController.php

--- pertemuan-4/rest-api/app/Http/Controllers/AnimalController.php
+++ pertemuan-4/rest-api/app/Http/Controllers/AnimalController.php
@@ -7,28 +7,29 @@ use Illuminate\Http\Request;
 class AnimalController extends Controller
 {
     public $animals =["Kucing", "Ayam", "Ikan"];
-        public function index(){
-            echo "Menampilkan data animals : \n";
+        public function index() 
+        {
+            echo "Menampilkan data animals" . PHP_EOL;
             foreach ($this->animals as $animal){
-                echo "- " . $animal . "\n";
-            }
+                echo $animal . PHP_EOL;
+                }
         }
-        public function store(Request $request){
-            echo "Menambahkan data animals - $request->nama";
-            $data = $request->only("animal");
-            array_push($this->animals, $data["animal"]);
-            echo $animal;
+        public function store(Request $request)
+         {
+            echo "Menambahkan binatang baru" . PHP_EOL;
+            $data = $request->only('nama');
+            array_push($this->animals, $data["nama"]);
+            $this->index();
         }
         public function update(Request $request, $id)
          {
-            
-            echo "Mengupdate data hewan index ke $id \n";
-            $data = $request->only('animal');
-            $this->animals[$id] = $data["animal"];
+            echo "Mengupdate data hewan index ke " . $id . PHP_EOL;
+            $data = $request->only('nama');
+            $this->animals[$id] = $data["nama"];
             $this->index();
         }
         public function destroy($id) {
-            echo "Menghapus data hewan index ke " . $id ."<br>";
+            echo "Menghapus data hewan index ke " . $id . PHP_EOL;
             unset($this->animals[$id]);
             $this->index();
         }
